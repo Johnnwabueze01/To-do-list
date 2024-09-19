150682caa5ee1fe5343c56605a2770d8892e5224
Nwabueze John <143359560+Johnnwabueze01@users.noreply.github.com>
2024-09-19 13:36:01 +0100

    Update todo.jsx

--- src/todo.jsx
+++ src/todo.jsx
@@ -1,24 +1,22 @@
 function Todo(props) {
-    return (
-      <li className="todo stack-small">
-        <div className="c-cb">
+  return (
+    <li className="todo stack-small">
+      <div className="c-cb">
+        <input id="todo-0" type="checkbox" defaultChecked={true} />
+        <label className="todo-label" htmlFor="todo-0">
+          {props.name}
+        </label>
+      </div>
+      <div className="btn-group">
+        <button type="button" className="btn">
+          Edit <span className="visually-hidden">{props.name}</span>
+        </button>
+        <button type="button" className="btn btn__danger">
+          Delete <span className="visually-hidden">{props.name}</span>
+        </button>
+      </div>
+    </li>
+  );
+}
 
-          <input id="todo-0" type="checkbox" defaultChecked={props.completed} />
-          <label className="todo-label" htmlFor="todo-0">
-            {props.name}
-          </label>
-        </div>
-        <div className="btn-group">
-          <button type="button" className="btn">
-            Edit <span className="visually-hidden">{props.name}</span>
-          </button>
-          <button type="button" className="btn btn__danger">
-            Delete <span className="visually-hidden">{props.name}</span>
-          </button>
-        </div>
-      </li>
-    );
-  }
-  
-  export default Todo;
-  
+export default Todo;
